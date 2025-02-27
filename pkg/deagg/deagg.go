21efd408dc514ef365ad75014a5344f68c5bc689
dichlorodiphen <dichlorodiphen@gmail.com>
2025-02-27 14:52:02 -0800

    Working

--- pkg/deagg/deagg.go
+++ pkg/deagg/deagg.go
@@ -9,7 +9,7 @@ import (
 
 var magic = []byte{0xF3, 0x89, 0x9A, 0xC2}
 
-const CHECKSUM_LEN = 15
+const CHECKSUM_LEN = 16
 
 func Deaggregate(data []byte) ([][]byte, error) {
 	if !bytes.Equal(data[0:len(magic)], magic) {
