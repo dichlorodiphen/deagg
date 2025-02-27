f7b28bdb6ef852af1e0df6e9f457731fbf614af1
dichlorodiphen <dichlorodiphen@gmail.com>
2025-02-27 14:37:35 -0800

    First draft

--- pkg/deagg/deagg.go
+++ pkg/deagg/deagg.go
@@ -1,1 +1,30 @@
 package deagg
+
+import (
+	"bytes"
+	"errors"
+
+	"google.golang.org/protobuf/proto"
+)
+
+var magic = []byte{0xF3, 0x89, 0x9A, 0xC2}
+
+const CHECKSUM_LEN = 15
+
+func Deaggregate(data []byte) ([][]byte, error) {
+	if !bytes.Equal(data[0:len(magic)], magic) {
+		return nil, errors.New("expected the first four bytes to match the KCL magic number")
+	}
+
+	var agg AggregatedRecord
+	if err := proto.Unmarshal(data[len(magic):(len(data)-CHECKSUM_LEN)], &agg); err != nil {
+		return nil, err
+	}
+
+	var records [][]byte
+	for _, r := range agg.Records {
+		records = append(records, r.GetData())
+	}
+
+	return records, nil
+}
--- pkg/deagg/reader.go
+++ pkg/deagg/reader.go
@@ -3,23 +3,48 @@ package deagg
 import (
 	"context"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/kinesis"
+	"github.com/aws/aws-sdk-go-v2/service/kinesis/types"
 )
 
 type Reader struct {
-	client *kinesis.Client
+	client     *kinesis.Client
+	streamName string
 }
 
-func NewReader(streamName string) (*kinesis.Client, error) {
+func NewReader(streamName string) (*Reader, error) {
 	cfg, err := config.LoadDefaultConfig(context.TODO())
 	if err != nil {
 		return nil, err
 	}
+	client := kinesis.NewFromConfig(cfg)
+	return &Reader{
+		client,
+		streamName,
+	}, nil
 
-	return kinesis.NewFromConfig()
 }
 
 func (r *Reader) Read(seq, shardId string) ([]byte, error) {
+	iter, err := r.client.GetShardIterator(context.TODO(), &kinesis.GetShardIteratorInput{
+		StreamName:             &r.streamName,
+		ShardId:                &shardId,
+		ShardIteratorType:      types.ShardIteratorTypeAtSequenceNumber,
+		StartingSequenceNumber: &seq,
+	})
+	if err != nil {
+		return nil, err
+	}
 
+	output, err := r.client.GetRecords(context.TODO(), &kinesis.GetRecordsInput{
+		ShardIterator: iter.ShardIterator,
+		Limit:         aws.Int32(1),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return output.Records[0].Data, nil
 }
