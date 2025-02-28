9f1bad7a9286e3a1f10fac55e35d21abcaad0452
dichlorodiphen <dichlorodiphen@gmail.com>
2025-02-27 17:28:36 -0800

    Handle aggregated and regular records

--- main.go
+++ main.go
@@ -9,22 +9,22 @@ import (
 
 func main() {
 	if len(os.Args) != 4 {
-		fmt.Fprintf(os.Stderr, "usage: %s <stream name> <sequence number> <shard id>", os.Args[0])
+		fmt.Fprintf(os.Stderr, "usage: %s <stream name> <sequence number> <shard id>\n", os.Args[0])
 		os.Exit(1)
 	}
 	streamName := os.Args[1]
 	seq := os.Args[2]
 	shardId := os.Args[3]
 
-	reader := Must(deagg.NewReader(streamName))
-	data := Must(reader.Read(seq, shardId))
-	records := Must(deagg.Deaggregate(data))
+	reader := must(deagg.NewReader(streamName))
+	data := must(reader.Read(seq, shardId))
+	records := must(deagg.DeaggregateOrNoOp(data))
 	for _, r := range records {
-		fmt.Print(string(r))
+		os.Stdout.Write(r)
 	}
 }
 
-func Must[T any](res T, err error) T {
+func must[T any](res T, err error) T {
 	if err != nil {
 		log.Fatal(err)
 	}
--- pkg/deagg/deagg.go
+++ pkg/deagg/deagg.go
@@ -9,11 +9,14 @@ import (
 
 var magic = []byte{0xF3, 0x89, 0x9A, 0xC2}
 
+// ErrInvalidFormat is the error returned by deaggregate when the data is not in the KPL aggregation format.
+var ErrInvalidFormat = errors.New("expected the first four bytes to match the KCL magic number")
+
 const CHECKSUM_LEN = 16
 
 func Deaggregate(data []byte) ([][]byte, error) {
 	if !bytes.Equal(data[0:len(magic)], magic) {
-		return nil, errors.New("expected the first four bytes to match the KCL magic number")
+		return nil, ErrInvalidFormat
 	}
 
 	var agg AggregatedRecord
@@ -26,5 +29,16 @@ func Deaggregate(data []byte) ([][]byte, error) {
 		records = append(records, r.GetData())
 	}
 
+	return records, nil
+}
+
+func DeaggregateOrNoOp(data []byte) ([][]byte, error) {
+	records, err := Deaggregate(data)
+	if err != nil {
+		if err == ErrInvalidFormat {
+			return [][]byte{data}, nil
+		}
+		return nil, err
+	}
 	return records, nil
 }
